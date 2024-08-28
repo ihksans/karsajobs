0efa87449b06a6b30fd58861fc92bd3bbcd60726
ihksan_ihksan <ihksan.dev@gmail.com>
2024-08-28 11:39:54 +0700

    feat: docker set up configuration

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,13 @@
+# Perintah untuk build Docker image dari berkas Dockerfile yang disediakan dengan nama <username-docker>/karsajobs:latest (untuk backend)
+docker build -t karsajobs .
+
+# Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag karsajobs ghcr.io/ihksans/karsajobs:latest
+
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal. 
+# Perlu memasukan password berupa access token GitHub Packages
+
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io -u ihksans --password-stdin
+
+# Mengunggah image ke Docker Hub / GitHub Packages.
+docker push ghcr.io/ihksans/karsajobs:latest
